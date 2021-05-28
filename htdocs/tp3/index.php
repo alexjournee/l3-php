1727b87dce937a8e7732879c727d88e032e2a251
alexandre.journee <alexandre.journee@gmail.com>
2021-05-28 16:02:17 +0200

    TP3 TP3 TP3

---
+++ htdocs/tp3/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Controller\HomeController;
+use App\Entity\Product;
+
+require_once "Autoload.php";
+Autoload::register();
+
+$product = new Product();
+$controller = new HomeController();
+
+var_dump($product);
+
+echo ('OK');
+
+$routeur = newRouteur();
+
